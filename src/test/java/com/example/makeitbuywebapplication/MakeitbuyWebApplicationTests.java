5e5021e4fbd9f129fec24073fd83e08423a8eb3d
Malebranchee <paho3998@mail.ru>
2024-02-01 16:09:45 +0600

    -Created project skeleton

---
+++ src/test/java/com/example/makeitbuywebapplication/MakeitbuyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makeitbuywebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeitbuyWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
